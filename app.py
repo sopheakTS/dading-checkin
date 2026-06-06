c5d7f29e140d512d1e575ffda32b156d6402032c
sopheakTS <sopheakts2019@gmail.com>
2026-06-06 22:17:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DaDing Employee System Running 🚀"
+    return "DaDing Bot Running 🚀"
 
 if name == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
